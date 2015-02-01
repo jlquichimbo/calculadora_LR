4bcae0c0176133a7c92dcabf910891a67ba7c9f1
joshe091 <joseluisquichimbo91@gmail.com>
2015-02-01 15:55:26 -0500

    Creando repositorio

--- CalculadoraLR/src/calculadoralr/CalculadoraLR.java
+++ CalculadoraLR/src/calculadoralr/CalculadoraLR.java
@@ -16,6 +16,7 @@ public class CalculadoraLR {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Holiiii");
     }
     
 }
